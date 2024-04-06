e5dfdeab79b665f866966d43ecb789790dd72197
Nagy Norbert-Botond <nagynorbi0303@gmail.com>
2024-04-06 21:03:35 +0200

    Add some stuff for 1b

--- src/app/tasks/1/B/task1-b.component.ts
+++ src/app/tasks/1/B/task1-b.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-task1-b',
   templateUrl: './task1-b.component.html',
   styleUrls: ['./task1-b.component.less']
 })
-export class Task1BComponent {
+export class Task1BComponent implements OnInit, OnChanges{
+  screenSize: string = '';
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.setScreenSize();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.setScreenSize();
+  }
+
+  setScreenSize() {
+    if (this.breakpointObserver.isMatched(Breakpoints.Small) || this.breakpointObserver.isMatched(Breakpoints.XSmall)) {
+      this.screenSize = 'Small';
+    } else if (this.breakpointObserver.isMatched(Breakpoints.Medium)) {
+      this.screenSize = 'Medium';
+    } else {
+      this.screenSize = 'Large';
+    }
+  }
+
 }
